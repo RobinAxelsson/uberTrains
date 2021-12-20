8531b01c053f1ebbe5d996309091b14a4d552a98
Albin <almalbin98@gmail.com>
2021-12-20 09:54:24 +0100

    Add files via upload

---
+++ dependencies.ps1
@@ -0,0 +1,9 @@
+﻿cd backend
+npm install -g typescript
+npm install -g ts-node
+npm install
+cd ..
+cd frontend
+npm install
+Write-Host -NoNewLine 'Skript färdigt... tryck valfri knapp för att avsluta';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
