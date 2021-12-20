b31f1bb9a07accd51078a4704394be9327c39446
Albin Alm <almalbin98@gmail.com>
2021-12-20 15:33:24 +0100

    Added asyncronous methods

--- backend/src/database/Repository.ts
+++ backend/src/database/Repository.ts
@@ -5,22 +5,29 @@ import { Booking } from "../models/Booking";
 
 export class Repository {
 
-AddBooking(bookingNumber:string, totalPrice:number) {
-createConnection().then(async connection => {
-
-const booking = new Booking();
-booking.bookingNumber = bookingNumber;
-booking.totalPrice = totalPrice;
-await connection.manager.save(booking);
-
+async AddBooking (bookingNumber:string, totalPrice:number) {
+    const booking = new Booking();
+    booking.bookingNumber = bookingNumber;
+    booking.totalPrice = totalPrice;
+    await createConnection().then(async connection => {
+        await connection.manager.save(booking);
+        await connection.close();
 });
 }
-RemoveBooking(booking:Booking) {
-    createConnection().then(async connection => {
 
+async RemoveBooking(booking:Booking) {
+    await createConnection().then(async connection => {
     await connection.manager.remove(booking);
-
+    await connection.close();
     });
 }
 
+async GetBookings() {
+    let bookings:Booking[] = []
+        await createConnection().then(async connection => {
+            bookings = await connection.manager.find(Booking);
+            await connection.close();
+        });
+        return bookings;
+}
 }
