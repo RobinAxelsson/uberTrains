75b048b135c5649f346ed50c475aca3acddb9524
malmz90 <70096644+malmz90@users.noreply.github.com>
2021-12-23 13:55:42 +0100

    navbar added, put travels in table, and alot more design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,16 @@ import "./App.css";
 import TravelForm from "./components/TravelForm";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import BookingReceipt from "./components/BookingReceipt";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<TravelForm />} />
-          <Route path="/receipt" element={<BookingReceipt />} />
-        </Routes>
-      </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<TravelForm />} />
+        <Route path="/receipt" element={<BookingReceipt />} />
+      </Routes>
     </Router>
   );
 }
--- frontend/src/components/TravelForm.js
+++ frontend/src/components/TravelForm.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+
+import ListTravels from "./ListTravels";
 
 const TravelForm = ({}) => {
   const [showTravels, setShowTravels] = useState(false);
@@ -32,70 +33,48 @@ const TravelForm = ({}) => {
     console.log(availableTravels);
   };
   return (
-    <div className="flex justify-center">
-      <form className="mt-6 justify-center" onSubmit={handleSubmit}>
-        <div>
-          <h2 className="font-bold text-4xl">Vart vill du resa?</h2>
-        </div>
-        <div className="mt-4">
-          <input
-            type="text"
-            className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-96 sm:text-sm border-gray-300 rounded-md"
-            placeholder="Från:"
-            required
-            value={start}
-            onChange={(e) => setStart(e.target.value)}
-          />
-        </div>
-        <div className="mt-1">
-          <input
-            type="text"
-            className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-96 sm:text-sm border-gray-300 rounded-md"
-            placeholder="Till:"
-            required
-            value={end}
-            onChange={(e) => setEnd(e.target.value)}
-          />
-        </div>
-        <div className="mt-1">
-          <input
-            type="text"
-            className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-96 sm:text-sm border-gray-300 rounded-md"
-            placeholder="Datum:"
-            required
-            value={date}
-            onChange={(e) => setDate(e.target.value)}
-          />
-        </div>
-        <button className="mt-1 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-          Fortsätt
-        </button>
-      </form>
-      {showTravels && (
-        <div>
-          {availableTravels &&
-            availableTravels.map((item) => (
-              <div>
-                <p>Tåg: {item.train}</p>
-                <p>TågId: {item.trainId}</p>
-                <p>
-                  {item.routeEvents.map((i) => (
-                    <div>
-                      {i.location}
-                      {i.dateTime}
-                      {i.eventType}
-                    </div>
-                  ))}
-                </p>
-                <Link to="receipt">
-                  <button className="mt-1 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                    Boka
-                  </button>
-                </Link>
-              </div>
-            ))}
-        </div>
-      )}
+    <div className="flex flex-col">
+      <div className="flex justify-center items-center">
+        <form className="mt-6 justify-center" onSubmit={handleSubmit}>
+          <div>
+            <h2 className="font-bold text-4xl">Vart vill du resa?</h2>
+          </div>
+          <div className="mt-4">
+            <input
+              type="text"
+              className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-96 sm:text-sm border-gray-300 rounded-md"
+              placeholder="Från:"
+              required
+              value={start}
+              onChange={(e) => setStart(e.target.value)}
+            />
+          </div>
+          <div className="mt-1">
+            <input
+              type="text"
+              className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-96 sm:text-sm border-gray-300 rounded-md"
+              placeholder="Till:"
+              required
+              value={end}
+              onChange={(e) => setEnd(e.target.value)}
+            />
+          </div>
+          <div className="mt-1">
+            <input
+              type="text"
+              className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-96 sm:text-sm border-gray-300 rounded-md"
+              placeholder="Datum:"
+              required
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            />
+          </div>
+          <button className="mt-1 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            Fortsätt
+          </button>
+        </form>
+      </div>
+      {showTravels && <ListTravels availableTravels={availableTravels} />}
     </div>
   );
 };
