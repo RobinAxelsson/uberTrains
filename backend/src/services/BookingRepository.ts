74354cd9d9132f096306ae453efc9e86414ede71
RobinAxelsson <axelsson.robin@outlook.com>
2021-12-23 16:40:09 +0100

    repository tests

---
+++ backend/src/services/BookingRepository.ts
@@ -0,0 +1,115 @@
+import "reflect-metadata";
+import {Booking} from "../models/Booking"
+import {Guid} from "./UtilityFunctions";
+import { createConnection } from 'typeorm';
+
+//Single responsibility
+export class BookingRepository {
+
+// private async Exists(bookingNumber:string){
+   
+//     return this.GetSingle(bookingNumber) != null;
+// }
+
+// async GetSingle(bookingNumber:string) {
+//     let booking;
+//      booking = (await this.GetAll()).find(obj => obj.BookingNumber == bookingNumber);
+//      return booking;
+// }
+
+//Create
+//Find
+//Update
+//Delete
+
+async Create(booking:Booking) {
+    return await createConnection().then(async connection => {
+            let response = await connection.manager.save(booking).catch(async err => {
+                console.log(JSON.stringify(err));
+                return null;
+            });
+            console.log(JSON.stringify(response));
+            await connection.close();
+            return response;
+        }).catch(async err => {
+            await console.log(JSON.stringify(err));
+            return null;
+        });
+}
+
+
+// async Update(booking:Booking) {
+//     if((await this.Exists(booking.BookingNumber)) == false) {
+//         return false;
+//     }
+//         await createConnection().then(async connection => { 
+//         await connection.manager.update(Booking, booking.Id, booking).catch(async err => {
+//             console.log(JSON.stringify(err));
+//             await connection.close();
+//             return false;
+//         });
+//         await connection.close();
+//     }).catch(async err => {
+//         console.log(JSON.stringify(err));
+//         return false;
+//     });
+//     return true;
+// }
+async Delete(booking:Booking) {
+    return await createConnection().then(async connection => {
+    let response = await connection.manager.remove(booking).catch(async err => {
+        console.log(JSON.stringify(err));
+        await connection.close();
+        return null;
+    });
+    console.log(JSON.stringify(response));
+    await connection.close();
+
+    return response;
+    }).catch(err => {
+        console.log(JSON.stringify(err));
+        return null;
+    });
+}
+// async RemoveBookingByBookingNumber(bookingNumber:string[] | string) {
+//     let connection = await createConnection();
+//     if(!Array.isArray(bookingNumber)) {
+//         let success = true;
+    
+//         let booking = this.GetSingle(bookingNumber);
+//         if(booking == null) return false;
+//         await createConnection().then(async connection => {
+//                 await connection.manager.remove(booking).catch(async err => {
+//                     console.log(JSON.stringify(err));
+//                     success = false;
+//                 });
+                
+//             await connection.close();
+//             }).catch(err => {
+//                 console.log(JSON.stringify(err));
+//                 success = false;
+//             });
+//         return success;
+//     }
+
+//     else {
+        
+//     }
+//     }
+  
+
+// async GetAll() {
+//     let bookings:Booking[] = []
+//         await createConnection().then(async connection => {
+//             bookings = await connection.manager.find(Booking).catch(err => {
+//             return [];
+//             });
+//             await connection.close();
+//         }).catch(err => {
+//             console.log(JSON.stringify(err));
+//         });
+//         return bookings;
+// }
+}
+
+//#endregion Bookings
--- backend/src/tests/unitTest/Repository.test.ts
+++ backend/src/tests/unitTest/Repository.test.ts
@@ -1,6 +1,53 @@
+import { Booking } from '../../models/Booking';
+import { BookingRepository } from '../../services/BookingRepository';
+import { Guid } from '../../services/UtilityFunctions';
 function sum(a: number, b: number) {
   return a + b;
 }
+
+
 test("adds 1+2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+function getBookingRandomBookingNumb(){
+  let booking = new Booking();
+  booking.BookingNumber = Guid.newGuid();
+  booking.StartStation = "Goteborg";
+  booking.EndStation = "Stockholm";
+  booking.LocalDateTime = new Date().toUTCString();
+  booking.PurchasedTickets = 3;
+  booking.TotalPrice = 133769;
+  return booking;
+}
+function getTypedBookingNumb(){
+  var booking = getBookingRandomBookingNumb();
+  booking.BookingNumber = "static-guid-booking-number";
+  return booking;
+}
+// test("Add booking expect success", async () =>{
+//   let bookingRepo = new BookingRepository();
+//   let booking = getBookingRandomBookingNumb();
+//   let actual = await bookingRepo.Create(booking);
+//   console.log(JSON.stringify(actual));
+//   expect(actual).toBe(booking);
+  
+// })
+
+// test("Delete booking expect success", async () =>{
+//   let bookingRepo = new BookingRepository();
+//   let booking = getTypedBookingNumb();
+//   await bookingRepo.Create(booking);
+//   let actual = await bookingRepo.Delete(booking);
+
+//   expect(actual).toBe(true);
+// })
+
+test("Delete booking expect fail", async () =>{
+  let bookingRepo = new BookingRepository();
+  let booking = getTypedBookingNumb();
+  await bookingRepo.Create(booking);
+  let actual = await bookingRepo.Delete(booking);
+
+  expect(actual).toBe(false);
+})
