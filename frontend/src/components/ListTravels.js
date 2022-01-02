b5c11ff66007cdb2c7c035f1cf79bb8c70f0412d
malmz90 <70096644+malmz90@users.noreply.github.com>
2022-01-02 13:51:06 +0100

    Update ListTravels.js

--- frontend/src/components/ListTravels.js
+++ frontend/src/components/ListTravels.js
@@ -95,6 +95,9 @@ const ListTravels = ({ availableTravels }) => {
         </div>
       </div>
       <Seats train={seats} />
+      <button className="mt-1 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+        Betala
+      </button>
     </div>
   );
 };
