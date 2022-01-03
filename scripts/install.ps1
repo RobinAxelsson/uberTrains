37e3d99bdee90be59d0079e9a3448aae224a0426
Albin <almalbin98@gmail.com>
2022-01-03 16:39:42 +0100

    Added package upgrading

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -3,9 +3,11 @@ npm install -g typescript
 npm install -g ts-node
 npm install -g typeorm
 npm install
+npm upgrade
 cd ..
 cd frontend
 npm install
+npm upgradde
 cd ..
 Write-Host -NoNewLine 'Skript färdigt... tryck valfri knapp för att avsluta';
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
