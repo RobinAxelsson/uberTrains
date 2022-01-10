6f4c6143ae8878c0a8fd7f1a2cd3b1646b5747c5
RobinAxelsson <axelsson.robin@outlook.com>
2022-01-10 12:20:43 +0100

    formatting unittests

--- backend/src/tests/TypeOrmRepostitory.test.ts
+++ backend/src/tests/TypeOrmRepostitory.test.ts
@@ -36,142 +36,138 @@ afterEach(() => {
   return conn.close();
 });
 
-test('Calculate prize JKPNG-STHLM', async () => {
-  await seed();
-  const priceCalculator = new PriceCalculator();
-  let startCoords = {
-    //JKPNG
-    latitude: 57.7825634,
-    longitude: 14.165719,
-  };
-  let endCoords = {
-    //Sthlm
-    latitude: 59.3251172,
-    longitude: 18.0710935,
-  };
-  let distance = priceCalculator.calculateDistance(
-    startCoords.latitude,
-    startCoords.longitude,
-    endCoords.latitude,
-    endCoords.longitude,
-  );
-  let price = priceCalculator.calculatePrice(distance, 0.8, 2, 1);
-  expect(distance).toStrictEqual(284.08);
-  expect(price).toStrictEqual(454.53);
-});
-test('Calculate prize witch CalculateDto JKPNG-STHLM', async () => {
-  await seed();
-  const bookingManager = new BookingManager(new PaymentManagerStub(), new mailServiceStub());
-
-  const calculateDto = {
-    travelPlanId: 1,
-    startRouteEventId: 3,
-    endRouteEventId: 4,
-    amount: 1,
-  } as GetPriceDto;
-
-  const price = await bookingManager.getPriceForBooking(calculateDto);
-  expect(price).toStrictEqual(454.53);
-});
-test('Get FULL travelPlan by start, stop, date JKPNG-STHLM', async () => {
-  await seed();
-  const data = await new TravelPlanner().getFullTravelPlanByStartStopDate(
-    'jonkoping',
-    'stockholm',
-    '2012-04-23',
-  );
-  expect(data?.map((x) => x.id)).toStrictEqual([1]);
-  expect(data?.map((x) => x.priceModel.priceConstant)).toStrictEqual([2]);
-});
-test('Get FULL travelPlan by start, stop, date STHLM-JKPNG, expect empty array', async () => {
-  await seed();
-  const data = await new TravelPlanner().getFullTravelPlanByStartStopDate(
-    'stockholm',
-    'jonkoping',
-    '2012-04-23',
-  );
-  expect(data).toStrictEqual([]);
-});
-test('TravelPlanner GetFullTravelPlanById, Load seeded travelplan id 1 expect 4 routeEvents', async () => {
-  await seed();
-  const travelPlan = await new TravelPlanner().getFullTravelPlanById(1);
-  expect(travelPlan.routeEvents.length).toBe(4);
-  expect(travelPlan.trainUnits.length).toBe(2);
-  expect(travelPlan.trainUnits[0].seats.length).toBe(2);
-  expect(travelPlan.trainUnits[1].seats.length).toBe(2);
-});
-
-test('Find RouteEvents between assert all', async () => {
-  await seed();
-  let events = await RouteEvent.find({
-    relations: ['travelPlan'],
-    where: [
-      {
-        dateTime: Between('2012-04-23', '2012-04-24'),
-        location: In(['goteborg', 'stockholm']),
-      },
-    ],
+describe('Price calculation', () => {
+  test('Calculate prize JKPNG-STHLM', async () => {
+    await seed();
+    const priceCalculator = new PriceCalculator();
+    let startCoords = {
+      //JKPNG
+      latitude: 57.7825634,
+      longitude: 14.165719,
+    };
+    let endCoords = {
+      //Sthlm
+      latitude: 59.3251172,
+      longitude: 18.0710935,
+    };
+    let distance = priceCalculator.calculateDistance(
+      startCoords.latitude,
+      startCoords.longitude,
+      endCoords.latitude,
+      endCoords.longitude,
+    );
+    let price = priceCalculator.calculatePrice(distance, 0.8, 2, 1);
+    expect(distance).toStrictEqual(284.08);
+    expect(price).toStrictEqual(454.53);
   });
-  //console.log(JSON.stringify({QuerybuilderBetweenAssertAll: events}, null, '\t'));
-  expect(events.length).toBe(2);
+  test('Calculate prize witch CalculateDto JKPNG-STHLM', async () => {
+    await seed();
+    const bookingManager = new BookingManager(new PaymentManagerStub(), new mailServiceStub());
+
+    const calculateDto = {
+      travelPlanId: 1,
+      startRouteEventId: 3,
+      endRouteEventId: 4,
+      amount: 1,
+    } as GetPriceDto;
+
+    const price = await bookingManager.getPriceForBooking(calculateDto);
+    expect(price).toStrictEqual(454.53);
+  });
 });
 
-test('As user I want to be able to book seats', async () => {
-  await seed();
+describe('SQLite interaction with TypeOrm API', () => {
+  describe('api/journey, Fetching TravelPlane by start, stop, date', () => {
+    test('Get FULL travelPlan by start, stop, date JKPNG-STHLM', async () => {
+      await seed();
+      const data = await new TravelPlanner().getFullTravelPlanByStartStopDate(
+        'jonkoping',
+        'stockholm',
+        '2012-04-23',
+      );
+      expect(data?.map((x) => x.id)).toStrictEqual([1]);
+      expect(data?.map((x) => x.priceModel.priceConstant)).toStrictEqual([2]);
+    });
+    test('Get FULL travelPlan by start, stop, date STHLM-JKPNG, expect empty array', async () => {
+      await seed();
+      const data = await new TravelPlanner().getFullTravelPlanByStartStopDate(
+        'stockholm',
+        'jonkoping',
+        '2012-04-23',
+      );
+      expect(data).toStrictEqual([]);
+    });
+  });
+  describe('api/travelPlan', () => {
+    test('TravelPlanner GetFullTravelPlanById, Load seeded travelplan id 1 expect 4 routeEvents', async () => {
+      await seed();
+      const travelPlan = await new TravelPlanner().getFullTravelPlanById(1);
+      expect(travelPlan.routeEvents.length).toBe(4);
+      expect(travelPlan.trainUnits.length).toBe(2);
+      expect(travelPlan.trainUnits[0].seats.length).toBe(2);
+      expect(travelPlan.trainUnits[1].seats.length).toBe(2);
+    });
+  });
 
-  expect((await Booking.find()).length).toBe(0);
-  expect((await Seat.find()).length).toBe(4);
+  describe('api/book booking seats', () => {
+    test('As user I want to be able to book seats', async () => {
+      await seed();
 
-  const bookingDto = {
-    travelPlanId: 1,
-    seatIds: [3, 4],
-    routeEventIds: [1, 2, 3, 4],
-    stripeInfo: {
-      id: 'stripe_1234',
-      email: 'post@man.se',
-      name: 'KalleBanan',
-    },
-  } as BookingDto;
-  const bookingManager = new BookingManager(new PaymentManagerStub(), new mailServiceStub());
+      expect((await Booking.find()).length).toBe(0);
+      expect((await Seat.find()).length).toBe(4);
 
-  const booking = await bookingManager.book(bookingDto);
+      const bookingDto = {
+        travelPlanId: 1,
+        seatIds: [3, 4],
+        routeEventIds: [1, 2, 3, 4],
+        stripeInfo: {
+          id: 'stripe_1234',
+          email: 'post@man.se',
+          name: 'KalleBanan',
+        },
+      } as BookingDto;
+      const bookingManager = new BookingManager(new PaymentManagerStub(), new mailServiceStub());
 
-  const seats = (await createQueryBuilder(Seat)
-    .leftJoinAndSelect('Seat.booking', 'Booking')
-    .where('Seat.id IN (:...ids)', { ids: bookingDto.seatIds })
-    .getMany()) as Seat[];
+      const booking = await bookingManager.book(bookingDto);
 
-  const dbBooking = (await createQueryBuilder(Booking)
-    .leftJoinAndSelect('Booking.bookedSeats', 'Seat')
-    .where('Booking.id = :id', { id: booking.id })
-    .getOne()) as Booking;
+      const seats = (await createQueryBuilder(Seat)
+        .leftJoinAndSelect('Seat.booking', 'Booking')
+        .where('Seat.id IN (:...ids)', { ids: bookingDto.seatIds })
+        .getMany()) as Seat[];
 
-  // console.log(JSON.stringify({Seats: seats}, null, '\t'));
-  // console.log(JSON.stringify({BookingWithSeats: booking}, null, '\t'));
-  expect(seats.every((x) => x.booking !== null)).toBeTruthy();
-  expect(dbBooking?.bookedSeats[0]).not.toBeNull();
-  expect(dbBooking?.bookedSeats[1]).not.toBeNull();
-  expect(booking.stripeId).toBe('stripe_1234');
-});
+      const dbBooking = (await createQueryBuilder(Booking)
+        .leftJoinAndSelect('Booking.bookedSeats', 'Seat')
+        .where('Booking.id = :id', { id: booking.id })
+        .getOne()) as Booking;
 
-test('As user I dont want to be able to book occupied seats', async () => {
-  await seed();
+      // console.log(JSON.stringify({Seats: seats}, null, '\t'));
+      // console.log(JSON.stringify({BookingWithSeats: booking}, null, '\t'));
+      expect(seats.every((x) => x.booking !== null)).toBeTruthy();
+      expect(dbBooking?.bookedSeats[0]).not.toBeNull();
+      expect(dbBooking?.bookedSeats[1]).not.toBeNull();
+      expect(booking.stripeId).toBe('stripe_1234');
+    });
 
-  const bookingDto = {
-    travelPlanId: 1,
-    seatIds: [1, 2],
-    routeEventIds: [1, 2, 3, 4],
-    stripeInfo: {
-      id: 'stripe_1234',
-      email: 'post@man.se',
-      name: 'KalleBanan',
-    },
-  } as BookingDto;
-  const bookingManager = new BookingManager(new PaymentManagerStub(), new mailServiceStub());
+    test('As user I dont want to be able to book occupied seats', async () => {
+      await seed();
 
-  await bookingManager.book(bookingDto);
+      const bookingDto = {
+        travelPlanId: 1,
+        seatIds: [1, 2],
+        routeEventIds: [1, 2, 3, 4],
+        stripeInfo: {
+          id: 'stripe_1234',
+          email: 'post@man.se',
+          name: 'KalleBanan',
+        },
+      } as BookingDto;
+      const bookingManager = new BookingManager(new PaymentManagerStub(), new mailServiceStub());
 
-  await expect(bookingManager.book(bookingDto)).rejects.toThrow(
-    'All seats are not free with the selected options',
-  );
+      await bookingManager.book(bookingDto);
+
+      await expect(bookingManager.book(bookingDto)).rejects.toThrow(
+        'All seats are not free with the selected options',
+      );
+    });
+  });
 });
