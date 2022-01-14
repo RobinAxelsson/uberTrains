b31136e86f73bfaf5368bbfdd8186fdcb62ebcd1
RobinAxelsson <axelsson.robin@outlook.com>
2022-01-14 12:59:09 +0100

    small refactor listTravels

--- frontend/src/components/ListTravels.js
+++ frontend/src/components/ListTravels.js
@@ -1,23 +1,18 @@
 import { extractTravelListData } from '../services/Utilities';
 
-const TableHeader = (title) => (
-  <th
-    scope="col"
-    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider "
-  >
+const TableHeader = (headerClass, title) => (
+  <th scope="col" className={headerClass}>
     {title}
   </th>
 );
 
 const cell = (cellFont, vals) => (
   <td className={cellFont}>
-    {vals.map(x => (
-      <div>
-        {x}
-      </div>
+    {vals.map((x) => (
+      <div>{x}</div>
     ))}
   </td>
-)
+);
 
 const ListTravels = ({
   startStation: [[start]],
@@ -35,15 +30,21 @@ const ListTravels = ({
           <div className="w-full min-w-full shadow border-b border-gray-200 sm:rounded-lg bg-white bg-opacity-75">
             <table className="min-w-full divide-y divide-gray-200">
               <thead className="bg-gray-50">
-                <tr>
-                  {TableHeader('Resa')}
-                  {TableHeader('Tid')}
-                  {TableHeader('Datum')}
-                  {TableHeader('Restid')}
-                  {TableHeader('Tåg')}
-                  {TableHeader('Pris')}
-                  {TableHeader('')}
-                </tr>
+                {(() => {
+                  const headerClass =
+                    'px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider ';
+                  return (
+                    <tr>
+                      {TableHeader(headerClass, 'Resa')}
+                      {TableHeader(headerClass, 'Tid')}
+                      {TableHeader(headerClass, 'Datum')}
+                      {TableHeader(headerClass, 'Restid')}
+                      {TableHeader(headerClass, 'Tåg')}
+                      {TableHeader(headerClass, 'Pris')}
+                      {TableHeader(headerClass, '')}
+                    </tr>
+                  );
+                })()}
               </thead>
               <tbody>
                 {availableTravels &&
@@ -55,25 +56,31 @@ const ListTravels = ({
                       return data;
                     })
                     .map((item) => {
-                      const cellFont =
+                      const cellClass =
                         'px-6 py-4 whitespace-nowrap text-sm text-gray-900 font-medium';
                       return (
                         <tr key={item.id} className="border-t border-gray-200">
-                          {cell(cellFont, [item.startLocation, item.endLocation])}
-                          {cell(cellFont, [item.startDateString, item.endDateString])}
-                          {cell(cellFont, [item.dateTime])}
-                          {cell(cellFont, [item.travelTime])}
-                          {cell(cellFont, [item.tripName])}
-                          {cell(cellFont, [item.price])}
-                          
+                          {cell(cellClass, [
+                            item.startLocation,
+                            item.endLocation,
+                          ])}
+                          {cell(cellClass, [
+                            item.startDateString,
+                            item.endDateString,
+                          ])}
+                          {cell(cellClass, [item.dateTime])}
+                          {cell(cellClass, [item.travelTime])}
+                          {cell(cellClass, [item.tripName])}
+                          {cell(cellClass, [item.price])}
+
                           <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                             <button
+                              className="mt-1 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                               onClick={() => {
                                 setShowTravels(false);
                                 setShowSeats(true);
                                 setChoosenTravel(item.travelPlan);
                               }}
-                              className="mt-1 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                             >
                               Välj platser
                             </button>
